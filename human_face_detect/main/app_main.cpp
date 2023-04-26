bcf9e5152e7356d33e6019e1128ccdeb7f780ec9
ApipooP <apipoo.paleeya@gmail.com>
2023-04-26 19:25:45 +0700

    added more images

--- human_face_detect/main/app_main.cpp
+++ human_face_detect/main/app_main.cpp
@@ -1,12 +1,14 @@
 #include <stdio.h>
-#include "image.hpp"
 #include "human_face_detect_msr01.hpp"
 #include "human_face_detect_mnp01.hpp"
 #include "dl_tool.hpp"
 #include <string>
 
-#define TWO_STAGE 1 /*<! 1: detect by two-stage which is more accurate but slower(with keypoints). */
-/*<! 0: detect by one-stage which is less accurate but faster(without keypoints). */
+// Select only one image header file. Uncomment to active.
+// #include "mona.hpp"
+#include "vanGogh.hpp"
+// #include "people.hpp"
+
 
 void inferenceOneStage(bool showLatency = true) {
     dl::tool::Latency latency;
@@ -19,15 +21,17 @@ void inferenceOneStage(bool showLatency = true) {
         latency.print("Inference latency");
     }
 
+    std::string boxArgs = "";
     int i = 0;
     for (std::list<dl::detect::result_t>::iterator prediction = results.begin(); prediction != results.end(); prediction++, i++) {
-        std::string toolArgs = "-b \"(" + std::to_string(prediction->box[0]) + ", "
-            + std::to_string(prediction->box[1]) + ", "
-            + std::to_string(prediction->box[2]) + ", "
-            + std::to_string(prediction->box[3]) + ")\"";
+        for (int point = 0; point < 4; point++) {
+            boxArgs += std::to_string(prediction->box[point]);
+            if (point != 3) boxArgs += ", ";
+        }
+        if (std::next(prediction) != results.end()) boxArgs += ", ";
         printf("[%d] score: %f, box: [%d, %d, %d, %d]\n", i, prediction->score, prediction->box[0], prediction->box[1], prediction->box[2], prediction->box[3]);
-        printf("display_image.py arguments:\n%s\n\n", toolArgs.c_str());
     }
+    printf("display_image.py arguments:\n-b \"(%s)\"\n\n", boxArgs.c_str());
 }
 
 void inferenceTwoStage(bool showLatency = true) {
@@ -43,18 +47,22 @@ void inferenceTwoStage(bool showLatency = true) {
         latency.print("Inference latency");
     }
 
+    std::string boxArgs = "";
+    std::string keypointsArgs = "";
     int i = 0;
-
     for (std::list<dl::detect::result_t>::iterator prediction = results.begin(); prediction != results.end(); prediction++, i++) {
-        std::string toolArgs = "-b \"(" + std::to_string(prediction->box[0]) + ", "
-            + std::to_string(prediction->box[1]) + ", "
-            + std::to_string(prediction->box[2]) + ", "
-            + std::to_string(prediction->box[3]) + ")\"";
-
-        toolArgs += " -k \"(";
-        for (int i = 0; i <= 8; i++) toolArgs += std::to_string(prediction->keypoint[i]) + ", ";
-        toolArgs += std::to_string(prediction->keypoint[9]) + ")\"";
-
+        for (int point = 0; point < 4; point++) {
+            boxArgs += std::to_string(prediction->box[point]);
+            if (point != 3) boxArgs += ", ";
+        }
+        for (int point = 0; point < 10; point++) {
+            keypointsArgs += std::to_string(prediction->keypoint[point]);
+            if (point != 9) keypointsArgs += ", ";
+        }
+        if (std::next(prediction) != results.end()) {
+            boxArgs += ", ";
+            keypointsArgs += ", ";
+        }
 
         printf("[%d] score: %f, box: [%d, %d, %d, %d]\n", i, prediction->score, prediction->box[0], prediction->box[1], prediction->box[2], prediction->box[3]);
         printf("    left eye: (%3d, %3d), ", prediction->keypoint[0], prediction->keypoint[1]);
@@ -62,8 +70,8 @@ void inferenceTwoStage(bool showLatency = true) {
         printf("    nose: (%3d, %3d)\n", prediction->keypoint[4], prediction->keypoint[5]);
         printf("    mouth left: (%3d, %3d), ", prediction->keypoint[2], prediction->keypoint[3]);
         printf("mouth right: (%3d, %3d)\n", prediction->keypoint[8], prediction->keypoint[9]);
-        printf("display_image.py arguments:\n %s\n\n", toolArgs.c_str());
     }
+    printf("display_image.py arguments:\n -b \"(%s)\" -k \"(%s)\"\n\n", boxArgs.c_str(), keypointsArgs.c_str());
 }
 
 extern "C" void app_main(void) {
