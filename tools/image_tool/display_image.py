4febba40c8b76e4344b75d58efdc6c33d2568b0d
tleoutline <tleoutline@gmail.com>
2023-04-26 16:13:11 +0700

    changes display_image tool to take multiple boexs

--- tools/image_tool/display_image.py
+++ tools/image_tool/display_image.py
@@ -5,6 +5,7 @@ import argparse
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Model generator tool')
     parser.add_argument('-i', '--image', help='path to image')
+    parser.add_argument('-o', '--output', help='path to output')
     parser.add_argument('-b', '--box', help='box coordinate in \"(x1, y1, x2, y2)\"', default='None')
     parser.add_argument('-k', '--keypoints', help='keypoints \"(x1, y1, x2, y2, ... , xn, yn)\"', default='None')
     args = parser.parse_args()
@@ -14,16 +15,26 @@ if __name__ == '__main__':
         quit()
 
     image = cv2.imread(args.image)
-    box = eval(args.box)
+    output = args.output
     keypoints = eval(args.keypoints)
+    if args.box != 'None':
+        boxarg = eval(args.box)
+        boxes = []
+        for i in range(0, len(boxarg)//4):
+            boxes.append((boxarg[0 + i*4], boxarg[1 + i*4],
+                          boxarg[2 + i*4], boxarg[3 + i*4]))
 
-    if box:
-        image = cv2.rectangle(image, (box[0], box[1]), (box[2], box[3]), (255, 0, 0), 2)
+        for box in boxes:
+            image = cv2.rectangle(
+                image, (box[0], box[1]), (box[2], box[3]), (255, 0, 0), 2)
 
     if keypoints:
         keypoints = numpy.reshape(keypoints, (-1, 2))
         for keypoint in keypoints:
             image = cv2.circle(image, tuple(keypoint), 2, (0, 255, 0), 2)
 
-    cv2.imshow('example', image)
-    cv2.waitKey(0)
+    if output is not None:
+        cv2.imwrite(output, image)
+    else:
+        cv2.imshow('example', image)
+        cv2.waitKey(0)
